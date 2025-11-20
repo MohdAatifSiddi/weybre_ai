e394a9772780be3ea8c53303138d4763fbeb6b92
MohdAatifSiddi <mekotecnolagy@gmail.com>
2025-11-20 09:46:47 +0530

    Implemented the authentication

---
+++ app/(routes)/auth/_common/signin-form.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import { z } from "zod";
+import React, { useState } from "react";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { RiLoader2Line } from "@remixicon/react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const signInSchema = z.object({
+  email: z.email("Invalid email"),
+  password: z.string().min(6, "Password must be at least 6 chars"),
+});
+
+type SignInFormValues = z.infer<typeof signInSchema>;
+
+const SignInForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<SignInFormValues>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: SignInFormValues) {
+    await authClient.signIn.email(
+      {
+        email: values.email,
+        password: values.password,
+        callbackURL: "/home",
+      },
+      {
+        onRequest: () => setIsLoading(true),
+        onSuccess: () => {
+          setIsLoading(false);
+          router.replace("/home");
+        },
+        onError: (ctx) => {
+          setIsLoading(false);
+          toast.error(ctx.error.message);
+        },
+      }
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-6">
+      <Card className="!bg-transparent border-0">
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Welcome Back</CardTitle>
+          <CardDescription>Sign in to your Weybre AI account</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="john.doe@example.com"
+                        type="email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="******" type="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={isLoading}
+              >
+                {isLoading ? (
+                  <RiLoader2Line className="size-4 animate-spin" />
+                ) : (
+                  "Sign In"
+                )}
+              </Button>
+
+              <p className="text-sm text-center text-muted-foreground">
+                Don’t have an account?{" "}
+                <Link href="/auth/sign-up" className="underline hover:text-primary">
+                  Create account
+                </Link>
+              </p>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInForm;
--- app/(routes)/auth/sign-in/page.tsx
+++ app/(routes)/auth/sign-in/page.tsx
@@ -1,7 +1,19 @@
+import Logo from "@/components/logo";
 import React from "react";
+import SignUpForm from "../_common/signup-form";
+import SignInForm from "../_common/signin-form";
 
 const Page = () => {
-    return <div>SignIn</div>;
+    return (
+        <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <div className="relative flex w-full max-w-sm flex-col gap-6">
+                <div className="w-full flex items-center justify-center">
+                    <Logo />
+                </div>
+                <SignInForm />
+            </div>
+        </div>
+    )
 }
 
 export default Page;
